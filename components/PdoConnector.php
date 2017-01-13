7bc083454023dd66f64c4c07e45231462820790b
Sebastian Golian <sebastiangolian@gmail.com>
2017-01-13 14:29:26 +0100

    2017-01-13

--- components/PdoConnector.php
+++ components/PdoConnector.php
@@ -11,13 +11,35 @@ use sebastiangolian\php\base\Component;
  * 
  * PHP Component allows the use of PDO library
  */
-
 class PdoConnector extends Component
 {
+    /**
+     * PDO handler
+     * @var PDO 
+     */
     private $_pdo;
+    
+    /**
+     * List of query parameters 
+     * @var array 
+     */
     private $_parameters = [];
+    
+    /**
+     * Query to prepare
+     * @var string 
+     */
     private $_query;
     
+    /**
+     * PdoConnector object
+     * $pdo = new \sebastiangolian\php\components\PdoConnector('localhost','db','root','password');
+     * @param string $dbhost
+     * @param string $dbname
+     * @param string $user
+     * @param string $pass
+     * @param array $config
+     */
     public function __construct($dbhost, $dbname, $user, $pass, $config = []) 
     {
         parent::__construct($config);
@@ -31,6 +53,17 @@ class PdoConnector extends Component
         }
     }
     
+    /**
+     * Returns an array containing all of the result set rows or query row count
+     * Testing::vd($pdo->query("INSERT INTO service VALUES (1,'test', 'test', '1')"));
+     * Testing::vd($pdo->query("INSERT INTO service VALUES (:id,:name,:short_name,:type)",['id'=>2,'name'=>'test','short_name'=>'t','type'=>1]));
+     * Testing::vd($pdo->query('SELECT * FROM service'));
+     * Testing::vd($pdo->query("DELETE FROM service WHERE id=:id",['id'=>1]));
+     * @param string $query
+     * @param array $params
+     * @param int $fetchmode
+     * @return mixed
+     */
     public function query($query, $params = null, $fetchmode = PDO::FETCH_ASSOC)
     {
         $query = trim(str_replace("\r", " ", $query));
@@ -47,15 +80,20 @@ class PdoConnector extends Component
         }
     }
     
-    public function before($query, $parameters = "")
+    /**
+     * Tasks before query realisation
+     * @param string $query
+     * @param array $parameters
+     */
+    private function before($query, $parameters = "")
     {
         try {
-            
             $this->_query = $this->_pdo->prepare($query);
+            $this->_parameters = [];
             $this->addParameters($parameters);
-            
-            if (!empty($this->parameters)) {
-                foreach ($this->parameters as $value) {
+
+            if (!empty($this->_parameters)) {
+                foreach ($this->_parameters as $value) {
                     if(is_int($value[1])) {
                         $type = PDO::PARAM_INT;
                     } else if(is_bool($value[1])) {
@@ -76,11 +114,20 @@ class PdoConnector extends Component
         }
     }
     
+    /**
+     * Add parameter to list
+     * @param type $parameter
+     * @param type $value
+     */
     public function addParametr($parameter, $value)
     {
         $this->_parameters[sizeof($this->_parameters)] = [":" . $parameter , $value];
     }
 
+    /**
+     * Add parameters to list
+     * @param type $parameters
+     */
     public function addParameters($parameters)
     {
         if (empty($this->_parameters) && is_array($parameters)) {
@@ -91,11 +138,53 @@ class PdoConnector extends Component
         }
     }
     
+    /**
+     * Get id last insert row
+     * @return string
+     */
+    public function getLastInsertId()
+    {
+        return $this->_pdo->lastInsertId();
+    }
+    
+    /**
+     * Begin transaction
+     * @return boolean
+     */
+    public function beginTransaction()
+    {
+        return $this->_pdo->beginTransaction();
+    }
+    
+    /**
+     * Execute transaction
+     * @return boolean
+     */
+    public function executeTransaction()
+    {
+        return $this->_pdo->commit();
+    }
+    
+    /**
+     * Rollback of transaction
+     * @return boolean
+     */
+    public function rollBack()
+    {
+        return $this->pdo->rollBack();
+    }
+    
+    /**
+     * Close PDO connection
+     */
     public function close()
     {
         $this->_pdo = null;
     }
     
+    /**
+     * PdoConnector destructor
+     */
     public function __destruct() {
         $this->close();
     }
