c3d2421c9cfa595169be482b61b20ca5b2b55c7a
Sebastian Golian <sebastiangolian@gmail.com>
2017-11-09 21:50:05 +0100

    2017-11-09

--- base/Object.php
+++ base/Object.php
@@ -100,8 +100,8 @@ abstract class Object
      * will be implicitly called when executing `$value = $object->property;`.
      * @param string $name the property name
      * @return mixed the property value
-     * @throws UnknownPropertyException if the property is not defined
-     * @throws InvalidCallException if the property is write-only
+     * @throws Exception if the property is not defined
+     * @throws Exception if the property is write-only
      * @see __set()
      */
     public function __get($name)
@@ -123,8 +123,8 @@ abstract class Object
      * will be implicitly called when executing `$object->property = $value;`.
      * @param string $name the property name or the event name
      * @param mixed $value the property value
-     * @throws UnknownPropertyException if the property is not defined
-     * @throws InvalidCallException if the property is read-only
+     * @throws Exception if the property is not defined
+     * @throws Exception if the property is read-only
      * @see __get()
      */
     public function __set($name, $value)
@@ -169,7 +169,7 @@ abstract class Object
      * Note that if the property is not defined, this method will do nothing.
      * If the property is read-only, it will throw an exception.
      * @param string $name the property name
-     * @throws InvalidCallException if the property is read only.
+     * @throws Exception if the property is read only.
      * @see http://php.net/manual/en/function.unset.php
      */
     public function __unset($name)
@@ -178,7 +178,7 @@ abstract class Object
         if (method_exists($this, $setter)) {
             $this->$setter(null);
         } elseif (method_exists($this, 'get' . $name)) {
-            throw new InvalidCallException('Unsetting read-only property: ' . get_class($this) . '::' . $name);
+            throw new Exception('Unsetting read-only property: ' . get_class($this) . '::' . $name);
         }
     }
     
@@ -189,7 +189,7 @@ abstract class Object
      * will be implicitly called when an unknown method is being invoked.
      * @param string $name the method name
      * @param array $params method parameters
-     * @throws UnknownMethodException when calling unknown method
+     * @throws Exception when calling unknown method
      * @return mixed the method return value
      */
     public function __call($name, $params)
