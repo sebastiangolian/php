1a1e4d407a413784971b7fe31cff1615b8fda539
sebastiangolian <sebastiangolian@gmail.com>
2017-11-10 15:37:32 +0100

    2017-11-10

--- index.php
+++ index.php
@@ -4,5 +4,6 @@ require_once 'autoload.php';
 use sebastiangolian\php\helpers\Math;
 use sebastiangolian\php\soap\GeoIPService;
 
+//testowy wpis
 $soap = new GeoIPService();
 var_dump($getGeoIPResponse = $soap->GetGeolocationByIp("188.127.30.36"));
