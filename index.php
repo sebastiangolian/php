dd18e77124cf031f85172ebb3bbbdecf35dc29bf
Sebastian Golian <sebastiangolian@gmail.com>
2017-11-20 21:26:16 +0100

    2017-11-20

--- index.php
+++ index.php
@@ -8,11 +8,12 @@ use sebastiangolian\php\helpers\VarDumper;
 
 require_once 'base/Autoloader.php';
 
-
+/*
 $address = Address::create()->setStreet('Długa 28')->setPostcode('20-000')->setCity('Warszawa');
 $email = Email::create()->setEmail('test@o2.pl')->setType(Email::TYPE_HOME);
 $phone = Phone::create()->setNumber('777777777')->setType(Phone::TYPE_HOME);
 $woker = Worker::create()->setId(1)->setFirstname('Jan')->setLastname('Kowalski')->setGender(Worker::GENDER_MAN);
 $company = Company::create()->setId(1)->setName('ZOO z.o.o.');
-
 $woker->addAddress($address)->addEmail($email)->addPhone($phone)->addCompany($company)->toString();
+*/
+
