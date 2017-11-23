3b71fd876e140aad50680781c1ad18c986b644fb
sebastiangolian <sebastiangolian@gmail.com>
2017-11-23 14:49:17 +0100

    2017-11-23

--- index.php
+++ index.php
@@ -13,7 +13,18 @@ $company = Company::create()->setId(1)->setName('ZOO z.o.o.');
 $woker->addAddress($address)->addEmail($email)->addPhone($phone)->addCompany($company)->toString();
 */
 
+//  ID  KEY VALUE                   A   B   C
+//  1   A   A1                  1   A1  B1  B1
+//  1   B   B1                  2   A2  n   C2
+//  1   C   C1                  3   A3  B3  n
+//  2   A   A2
+//  2   B   null
+//  2   C   C2
+//  3   A   A3
+//  3   B   B3
+//  3   C   null
 
+/*
 $arr[1]['A'] = 'A1';
 $arr[1]['B'] = 'B1';
 $arr[1]['C'] = 'C1';
@@ -23,6 +34,25 @@ $arr[2]['C'] = 'C2';
 $arr[3]['A'] = 'A3';
 $arr[3]['B'] = 'B3';
 $arr[3]['C'] = 'null';
+*/
 
-VarDumper::dump($arr);
+$arr = [
+    ['1','A','A1'],  
+    ['1','B','B1'],  
+    ['1','C','C1'],  
+    ['2','A','A2'],  
+    ['2','B',null],  
+    ['2','C','C2'],  
+    ['3','A','A3'],
+    ['3','B','B3'],
+    ['3','C',null],  
+];
+
+$arrNew = array();
+foreach ($arr as $record)
+{
+    $arrNew[$record[0]][$record[1]] = $record[2];
+}
+
+VarDumper::dump($arrNew['2']['B']);
 
