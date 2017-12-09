1844cb0fca77d20fbeca0fb103d07086560cd3a6
Sebastian Golian <sebastiangolian@gmail.com>
2017-12-09 22:39:25 +0100

    2017-12-09

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 
+use sebastiangolian\php\components\SqliteCommand;
+use sebastiangolian\php\components\SqliteConnector;
+use sebastiangolian\php\helpers\VarDumper;
 
 require_once 'base/Autoloader.php';
+
+$sqlliteCommand = new SqliteCommand(SqliteConnector::getInstance('db/sqlite.db'));
+
+/*
+$sqlliteCommand->insert('customer',[
+    'firstname'=>'Michał',
+    'lastname' => 'Nowaczek',
+    'profile_id' => 1
+]);
+
+$sqlliteCommand->update('customer',['firstname'=>'Michał'],['customer_id'=>3]);
+$sqlliteCommand->delete('customer',['customer_id'=>5]);
+*/
+
+
+$sqlliteCommand->printTables(['customer','profile']);
